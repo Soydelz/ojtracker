712041e0d800d343f29f4f1b675c4424e157a21c
OJTracker <syydlzz@gmail.com>
2026-02-05 10:06:30 +0800

    Fix error handler - remove debug JSON response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,25 +27,6 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             // Log errors to stderr so they appear in Render logs
             error_log("LARAVEL ERROR: " . $e->getMessage());
-            error_log("FILE: " . $e->getFile() . " LINE: " . $e->getLine());
         });
-    }
-    
-    /**
-     * Render an exception into an HTTP response.
-     */
-    public function render($request, Throwable $e)
-    {
-        // Always show detailed errors for debugging
-        if (config('app.debug') || env('APP_DEBUG') === 'true' || env('APP_DEBUG') === true) {
-            return response()->json([
-                'error' => $e->getMessage(),
-                'file' => $e->getFile(),
-                'line' => $e->getLine(),
-                'trace' => $e->getTraceAsString()
-            ], 500);
-        }
-        
-        return parent::render($request, $e);
     }
 }
