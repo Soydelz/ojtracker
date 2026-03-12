64b2a540a3d70448ad98be9c96434cd4eee75873
OJTracker <syydlzz@gmail.com>
2026-03-12 10:31:01 +0800

    Fix PostgreSQL cached plan error: emulate prepared statements

--- config/database.php
+++ config/database.php
@@ -76,6 +76,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'require',
+            'options' => extension_loaded('pdo_pgsql') ? array_filter([
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ]) : [],
         ],
 
         'sqlsrv' => [
